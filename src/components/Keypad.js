b73f9ada636770d7421894f29b4c5bc91e150d31
Kodo Yousif <kodo.00257248@gmail.com>
2021-02-27 05:59:30 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -3,11 +3,15 @@ import React from 'react';
 export default class Keypad extends React.Component {
 
   
+handleInputPassword = () => console.log('Entering password...')
 
   render() {
     return (
       <div>
-        
+        <input 
+          type="password"
+          onKeyUp={this.handleInputPassword} 
+        />
       </div>
     )
   }
